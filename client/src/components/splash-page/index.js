36d93772e65a4a81dea2c2d8099bedb6e571f7f9
FrankCarvajal <franklin.a.carvajal@gmail.com>
2017-05-18 16:48:56 -0400

    basic frontend ugliness disintegration

--- client/src/components/splash-page/index.js
+++ client/src/components/splash-page/index.js
@@ -5,6 +5,7 @@ import './index.css';
 export default function SplashPage() {
     return <div className='header'>
                 <img src={senseiLogo} alt='senseiX' className="logo" />
+                <h1 className="what">LEARN JAPANESE THROUGH SPACED REPETITION</h1>
                 <a href={'/api/auth/github'} className="loginlink">Login / Get Started with Github</a>
             </div>
 }
