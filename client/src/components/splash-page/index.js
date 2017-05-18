20f8a8028b22c271164469673866adfa0b1cba67
FrankCarvajal <franklin.a.carvajal@gmail.com>
2017-05-18 16:55:15 -0400

    beautifying a tad for fun

--- client/src/components/splash-page/index.js
+++ client/src/components/splash-page/index.js
@@ -6,6 +6,8 @@ export default function SplashPage() {
     return <div className='header'>
                 <img src={senseiLogo} alt='senseiX' className="logo" />
                 <h1 className="what">LEARN JAPANESE THROUGH SPACED REPETITION</h1>
-                <a href={'/api/auth/github'} className="loginlink">Login / Get Started with Github</a>
+                <div className="loginbutton">
+                    <a href={'/api/auth/github'} className="loginlink">Login / Get Started with Github</a>
+                </div>  
             </div>
 }
