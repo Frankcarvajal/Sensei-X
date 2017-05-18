7612e0c5b9a5c62bc2805813a93076317ddcbd9f
FrankCarvajal <franklin.a.carvajal@gmail.com>
2017-05-18 17:43:22 -0400

    revisions to style and start of further redux work

--- client/src/reducers.js
+++ client/src/reducers.js
@@ -1,10 +1,14 @@
 import {combineReducers} from 'redux';
 import app from './components/app/reducers';
 import fetchQuestions from './components/questions-page/reducers';
+import fetchAnswers from './components/guess-form/reducer';
+import fetchCurrentQIndex from './components/guess-form/reducer';
 import guessForm from './components/guess-form/reducer';
 
 export default combineReducers({
     app,
     fetchQuestions,
+    fetchAnswers,
+    fetchCurrentQIndex,
     guessForm
 });
